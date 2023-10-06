9b7a6c9f6acbfa4856c1aaac2b5c553972402c84
千机楼 <qianjilou@qq.com>
2023-10-06 19:43:51 +0800

    Update index.js
    imuke test

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wx22da6f3cfc774d65",
+  appsecret: "4bbcd1e3bde32e74eb9385bc30c33774",
+  template_id: "MBTRjMDA3sYb41GQRBorhu7u4SAoVzht0hPj62u1qU4",
+  touser: "ohSj05hjEyN3KNDYiID-T2LTiriw",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "武汉",
+  first_date: "2023-10-01",
 };
